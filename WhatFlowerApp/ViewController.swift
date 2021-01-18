51de2ca6b0d8b719817bac7415b783cc9fc6ebbf
EgorMezhin <49487690+EgorMezhin@users.noreply.github.com>
2021-01-18 23:00:01 +0300

    Add base functionality

--- WhatFlowerApp/ViewController.swift
+++ WhatFlowerApp/ViewController.swift
@@ -6,11 +6,13 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     let imagePicker = UIImagePickerController()
-
+    
     @IBOutlet weak var imageView: UIImageView!
     
     
@@ -25,18 +27,46 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let userPickedImage = info[UIImagePickerController.InfoKey.editedImage]
-        
-        imageView.image = userPickedImage as? UIImage
-        imagePicker.dismiss(animated: true, completion: nil)
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage {
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Cannot convert to CIImage.")
+            }
+            
+            detect(image: ciImage)
+            
+            imageView.image = userPickedImage
+            imagePicker.dismiss(animated: true, completion: nil)
+        }
     }
     
+    func detect(image: CIImage) {
+        
+        guard let model = try? VNCoreMLModel(for: FlowerClassifier().model) else {
+            
+            fatalError("Cannot import model.")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { (request, error) in
+            
+            let classification = request.results?.first as? VNClassificationObservation
+        
+            self.navigationItem.title = classification?.identifier
+            
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try handler.perform([request])
+        } catch {
+            print(error)
+        }
+    }
     
-
     @IBAction func cameraButtonPressed(_ sender: UIBarButtonItem) {
         
         present(imagePicker, animated: true, completion: nil)
-        
     }
     
 }
